373206e2e20339835856afb0b6b1e30438006d76
Nolwazi Ngcobo <214553811@stu.ukzn.ac.za>
2016-03-16 11:23:09 +0200

    Implemint the move function.

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,11 +1,98 @@
 #include "TicTacToe.h"
+#include<iostream>
 using namespace std;
+#define ROWS 3
+#define COLUMNS 3
 
 TicTacToe::TicTacToe()
 {
 }
 
-
 TicTacToe::~TicTacToe()
 {
+}
+void TicTacToe::reset()
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLUMNS; j++)
+		{
+			grid[i][j] = ' ';
+		}
+	}
+}
+void TicTacToe::print()
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLUMNS; j++)
+		{
+			cout << grid[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+bool TicTacToe::move(int i, int j)
+{
+
+	int arr[9],k=0;
+	while (k < 9)
+	{
+		if (k % 2 == 0) cout << "Player 1 move" << endl;
+		else cout << "Player 2 move" << endl;
+		cout << "Enter position\n";
+		cin >> position;
+		arr[k] = position;
+		/*for (int m = 0; m < 9; m++)
+		{
+			if (arr[m] == position)
+				//cout << "illegal move!" << endl;
+				return false;
+			else
+				//cout << "move is legal"<<endl
+				return true;
+		}*/
+		if (position == 1)
+		{
+			i = 0, j = 0;
+		}
+		if (position == 2)
+		{
+			i = 0, j = 1;
+		}
+		if (position == 2)
+		{
+			i = 0, j = 2;
+		}
+		if (position == 4)
+		{
+			i = 1, j = 0;
+		}
+		if (position == 5)
+		{
+			i = 1, j = 1;
+		}
+		if (position == 6)
+		{
+			i = 1, j = 2;
+		}
+		if (position == 7)
+		{
+			i = 2, j = 0;
+		}
+		if (position == 8)
+		{
+			i = 2, j = 1;
+		}
+		if (position == 0)
+		{
+			i = 2, j = 2;
+		}
+	}
+	if (k % 2 == 0)
+	{
+		grid[i][j] == 'X';
+	}
+	else grid[i][j] == 'O';
+	k++;
 }
--- TicTacToe/TicTacToe.h
+++ TicTacToe/TicTacToe.h
@@ -3,13 +3,14 @@
 class TicTacToe
 {
 private:
-	TicTacToe();
-	~TicTacToe();
+	
 	int position; //where player wants to make a move
 	char player1;
 	char player2;
-	char grid[3][3];
+	int grid[3][3];
 public:
+	TicTacToe();
+	~TicTacToe();
 	void reset();
 	bool move(int, int);
 	void print();
